7e1ace5d0658547e991ebdb5352b2d9d3a3583f8
alexvedenyev <aleks.vedenyev@gmail.com>
2021-04-04 22:11:46 +0300

    made CLI calculator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-const calculate = (left, right, operator) => {
+const maths = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
+};
 
+const operators = Object.keys(maths);
+
+const isOperator = (operator) => operators.includes(operator);
+const isNumber = (num) => typeof num === 'number';
+const isNumbers = (nums) => nums.every(isNumber);
+const isValid = (left, right, operator) => isNumbers([left, right]) && isOperator(operator);
+const isNotValid = (...args) => !isValid(...args);
+
+const calculate = (left, right, operator) => {
+  if (isNotValid(left, right, operator)) {
+    return NaN;
+  };
+  const math = maths[operator];
+  return math(left, right);
 };
 
 const parse = (expression) => {
+  const clearExpression = expression.replace(' ', '');
+  const hasOperator = (operator) => clearExpression.includes(operator);
+  const [currentOperator] = operators.filter(hasOperator);
+  const operands = clearExpression.split(currentOperator);
 
+  if (operands.lenght === 1) {
+    const [right] = operands;
+    return [NaN, parseFloat(right), currentOperator]; 
+  };
+
+  const [left, right] = operands;
+  return [parseFloat(left), parseFloat(right), currentOperator];
 };
 
 export default (expression, acc = 0) => {
   const [left, right, operator] = parse(expression);
-  return calculate(left, right, operator);
+  const leftOrAcc = Number.isNaN(left) ? acc : left;
+  return calculate(leftOrAcc, right, operator);
 };
